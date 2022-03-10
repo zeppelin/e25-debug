3d8bdb2155d3bfb8263a51a82c276699f343f0c9
Gabor Babicz <gabor@gaborbabicz.com>
2022-03-10 10:33:02 +0100

    💥

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'e25/config/environment';
+import { deprecate } from '@ember/debug';
+
+console.log(deprecate);
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
